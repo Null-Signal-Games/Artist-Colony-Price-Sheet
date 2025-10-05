23c58e1884351730859fba1b5c0d4a9d1dd49a52
Jon Borgonia <jon.borgonia@gmail.com>
2025-10-05 14:09:43 -0700

    :sparkles: table renders csv
    - adds time last updated (in BST tz)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,33 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	export let data;
+</script>
+
+<h1>Artist Colony Price Sheet</h1>
+<p class="last-updated-container">
+	Last Updated:
+	<span class="last-updated-value">{data.lastUpdated}</span>
+	BST (UTC+1)
+</p>
+
+{#if data.csvData && data.csvData.length > 0}
+	<table>
+		<thead>
+			<tr>
+				{#each Object.keys(data.csvData[0]) as header}
+					<th>{header}</th>
+				{/each}
+			</tr>
+		</thead>
+		<tbody>
+			{#each data.csvData as row}
+				<tr>
+					{#each Object.values(row) as cell}
+						<td>{cell}</td>
+					{/each}
+				</tr>
+			{/each}
+		</tbody>
+	</table>
+{:else}
+	<p>No CSV data found.</p>
+{/if}
